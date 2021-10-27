0d7a131306b8c81158bfc94bde9fe59c4b9dc90b
Misako Watanabe <misakolani@gmail.com>
2021-10-27 16:28:34 +0200

    changed environment for local dev

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const ENDPOINT = "https://fullstack-todo-backend-misako.herokuapp.com";
-// export const ENDPOINT = "http://localhost:3001";
+// export const ENDPOINT = "https://fullstack-todo-backend-misako.herokuapp.com";
+export const ENDPOINT = "http://localhost:3001";
